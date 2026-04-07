98aabe560cc54b0607ceb5b74848580a9d03cb6c
DaniHogg <daniel.hogg1992@gmail.com>
2026-04-07 12:54:07 +1000

    fixing to run both JS and Py tests at once

--- playwright.config.js
+++ playwright.config.js
@@ -10,7 +10,7 @@ const { defineConfig, devices } = require('@playwright/test');
  * See https://playwright.dev/docs/test-configuration.
  */
 module.exports = defineConfig({
-  testDir: './tests',
+  testDir: './test-js',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
